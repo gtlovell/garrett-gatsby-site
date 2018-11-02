e2c62a39c58a634aff6631cf3d42a6d076b9d104
Garrett Lovell <seanhelmes@Garretts-MacBook-Pro-2.local>
2018-11-02 11:17:22 -0400

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/layout';
+
+const About = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. A tempore nam
+        alias perspiciatis neque id nulla doloribus aperiam voluptatum sint
+        incidunt illum dicta illo nemo fugit quae, itaque enim blanditiis. Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. A tempore nam alias
+        perspiciatis neque id nulla doloribus aperiam voluptatum sint incidunt
+        illum dicta illo nemo fugit quae, itaque enim blanditiis.
+      </p>
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  );
+};
+
+export default About;
